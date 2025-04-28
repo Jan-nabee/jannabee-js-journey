fc8a4b4e370770ef0460c86c68c0f7fa508d499a
Jan-nabee <ansrk0000@naver.com>
2025-04-28 16:47:10 +0900

    fix: set user info

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = "잔나비";
+const age = "32"
+const hobby = "차 마시기, 요가"
+
+const myIntro = `안녕하세요! 저는 ${name}이고, ${age}살이며, 취미는 ${hobby}입니다.`;
+
+document.getElementById("profile").innerText = myIntro;
