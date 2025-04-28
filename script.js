b9f5c8b459af62b6ab2b417e5cb2834135b1dc63
Jan-nabee <ansrk0000@naver.com>
2025-04-28 18:06:01 +0900

    fix: update letProfile id and nickname to Shannabee

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// const 사용 예시
 const name = "잔나비";
 const age = "32"
 const hobby = "차 마시기, 요가"
@@ -5,3 +6,8 @@ const hobby = "차 마시기, 요가"
 const myIntro = `안녕하세요! 저는 ${name}이고, ${age}살이며, 취미는 ${hobby}입니다.`;
 
 document.getElementById("profile").innerText = myIntro;
+
+
+// let 사용 예시
+let nickname = "샨나비(Shannabee)"
+document.getElementById("letProfile").innerText = `나를 담은 이름: ${nickname}`;
